313ab1eeff8a049b30679a9c207229b3e581c6cb
Ykris97 <chickanan@gmail.com>
2022-09-30 11:31:46 +0300

    Still trying to figure out how to loop it.

--- script.js
+++ script.js
@@ -28,50 +28,43 @@ const ComputerInput = getRandomItem(array);
 
 
 
+
 // The Matrix of the results and who wins // 
 
 function gameStructure () {
   
- if (UserInput === "STONE" && ComputerInput === "STONE"){
+ if (UserInput === "STONE" && ComputerInput === "STONE" || UserInput === "PAPER" && ComputerInput === "PAPER" || UserInput === "SCISSORS" && ComputerInput === "SCISSORS" ){
 
-    alert("It's a tie");
+    console.log("It's a tie");
 
- }else if (UserInput === "STONE" && ComputerInput === "PAPER"){
 
-     alert("You lose");
+ }else if (UserInput === "STONE" && ComputerInput === "PAPER" || UserInput === "PAPER" && ComputerInput === "SCISSORS" || UserInput === "SCISSORS" && ComputerInput === "STONE"){
 
- }else if (UserInput === "STONE" && ComputerInput === "SCISSORS"){
+     console.log("You lose");
 
-    alert("You win");
 
- }else if (UserInput === "PAPER" && ComputerInput === "PAPER"){
 
-    alert("It's a tie");
- }else if (UserInput === "PAPER" && ComputerInput === "STONE"){
+ }else if (UserInput === "STONE" && ComputerInput === "SCISSORS"|| UserInput === "PAPER" && ComputerInput === "STONE" || UserInput === "SCISSORS" && ComputerInput === "PAPER"){
 
-    alert("You win");
+    console.log("You win");
 
- }else if (UserInput === "PAPER" && ComputerInput === "SCISSORS"){
 
-    alert("You lose");
-
- }else if (UserInput === "SCISSORS" && ComputerInput === "STONE"){
-
-    alert("You lose");
-
- }else if (UserInput === "SCISSORS" && ComputerInput === "PAPER"){
-
-    alert("You win");      
-
- }else if (UserInput === "SCISSORS" && ComputerInput === "SCISSORS"){
-
-    alert("It's a tie");
  } else {
     
-    alert ("Check your input again")
+    console.log("Check your input again")
+
  }
  
 }
 
 
-gameStructure();
+let i =0
+
+do {
+   
+gameStructure(i); i++
+
+} 
+
+
+while (i < 5);
